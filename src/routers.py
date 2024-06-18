92361baa716b88978ed55d6ba4ba75a6c5a02e74
Alex Bridges <feasarilly@gmail.com>
2024-06-18 16:12:45 +0700

    hub!

---
+++ src/routers.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Depends, UploadFile
+from sqlalchemy.orm import Session
+from database import get_db
+import services
+
+router = APIRouter()
+
+
+@router.post("/document/", tags=["document"])
+async def upload(data: UploadFile = None, db: Session = Depends(get_db)):
+    return services.upload_doc(data, db)
+
+
+@router.delete("/document/", tags=["document"])
+async def delete(id: int = None, db: Session = Depends(get_db)):
+    return services.remove_doc(id, db)
+
+
+@router.put("/document/", tags=["document"])
+async def analys(id: int = None, db: Session = Depends(get_db)):
+    return services.analyse_doc(id, db)
+
+
+@router.get("/document/", tags=["document"])
+async def extract(id: int = None, db: Session = Depends(get_db)):
+    return services.get_text(id, db)
+
+@router.get("/api/", tags=["healthcheck"])
+async def health_check():
+    return {"status changed": True}
