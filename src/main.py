7a47c070bbe35c25112f792b88ee7dd85531b944
Alex Bridges <feasarilly@gmail.com>
2024-06-20 16:11:07 +0700

    WITH EASE

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import routers
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
-app.include_router(routers.router, prefix="/document")
+app.include_router(routers.router, prefix="")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
--- src/routers.py
+++ src/routers.py
@@ -5,27 +5,29 @@ from services import *
 
 router = APIRouter()
 
+# document
 
-@router.post("/document/", tags=["document"])
+@router.post("/upload/", tags=["document"])
 async def upload(data: UploadFile = None, db: Session = Depends(get_db)):
     return upload_doc(data, db)
 
 
-@router.delete("/document/", tags=["document"])
-async def delete(id: int = None, db: Session = Depends(get_db)):
-    return remove_doc(id, db)
-
-
-@router.put("/document/", tags=["document"])
+@router.put("/analys/", tags=["document"])
 async def analys(id: int = None, db: Session = Depends(get_db)):
     return analyse_doc(id, db)
 
 
-@router.get("/document/", tags=["document"])
+@router.get("/extract/", tags=["document"])
 async def extract(id: int = None, db: Session = Depends(get_db)):
     return get_text(id, db)
 
 
-@router.get("/api/", tags=["healthcheck"])
+@router.delete("/delete/", tags=["document"])
+async def delete(id: int = None, db: Session = Depends(get_db)):
+    return remove_doc(id, db)
+
+# log
+
+@router.get("/healthcheck/", tags=["log"])
 async def health_check():
     return {"status": True}
